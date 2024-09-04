6ee334abc8ac1766109712fbba4b695592882ea3
Carl Dalebout <cadalebout1@ccis.edu>
2024-09-04 13:30:37 -0500

    finished with a01q06

--- a/a01/a01q06notDone/main.py
+++ a/a01/a01q06notDone/main.py
@@ -3,11 +3,60 @@
 # Author: Carl Dalebout
 
 def crossover(xs=None, ys=None, index0=0, index1=0):
+    
+    us = []
+    vs = []
 
     if isinstance(xs, str) and isinstance(ys, str):
-        pass
+        for i  in range(index0):
+            us.append(xs[i])
+            vs.append(ys[i])
+
+        if index1 <= len(xs) and index1 <= len(ys): # case for where index1 is less then the len of both xs and ys
+            for i in range(index0, index1):
+                us.append(ys[i])
+                vs.append(xs[i])
+        
+        elif index1 <=len(xs):                      # case for where index1 is less then the len xs but greater then the len ys
+            for i in range(index0, index1):
+                vs.append(xs[i])
+            for i in range(index0, len(ys)):
+                us.append(ys[i])
+
+        elif index1 <= len(ys):                     # case for where index1 is greater then the len of xs but less then the len of ys
+            for i in range(index0, len(xs)):
+                vs.append(xs[i])
+            for i in range(index0, len(index1)):
+                us.append(ys[i])
+            
+        else:                                      # case for where index1 is greater then the len of both xs and ys
+            for i in range(index0, len(xs)):
+                vs.append(xs[i])
+            for i in range(index0, len(ys)):
+                us.append(ys[i])
+            
+
+        for i in range(index1, len(xs)):
+            us.append(xs[i])
+        
+        for i in range(index1, len(ys)):
+            vs.append(ys[i])
+        
+        return us, vs
+    
     elif isinstance(xs, list) and isinstance(ys, list):
-        pass    
+        for i  in range(index0):
+            us.append(xs[i])
+            vs.append(ys[i])
+        for i in range(index0, index1):
+            us.append(ys[i])
+            vs.append(xs[i])
+        for i in range(index1, len(xs)):
+            us.append(xs[i])
+        for i in range(index1, len(ys)):
+            vs.append(ys[i])
+        return us, vs 
+    
     else:
         raise ValueError("ERROR in crossover: xs=%s, ys=%s" % (xs, ys))
 
