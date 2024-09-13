4575db1c46581d1e4bb389ab5937933c96f40759
Carl <cadalebout1@ccis.edu>
2024-09-13 17:25:26 -0500

    Finished DLList aka a02q01

--- a/a02/a02q01/main.py
+++ a/a02/a02q01/main.py
@@ -3,15 +3,17 @@
 # Author: Carl Dalebout
 
 import DLList
-import DLNode
-
 
 if __name__ == '__main__':
-    xs = [1,2,3]
-    ys = [3,2,1]
-    a = DLList.DLList(xs)
-    b = DLList.DLList(ys)
-
-    print(len(a))
-    print(a.head)
-    print(a.get_list())
+    xs = DLList.DLList(); print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
+    xs.insert_head(5); print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
+    xs.insert_head(2); print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
+    xs.insert_tail(6); print(xs.head, xs, len(xs), xs.is_empty())
+    xs.head = 1234; print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
+    xs.tail = 5678; print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
+    x = xs.delete_head()
+    print(x)
+    print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
+    x = xs.delete_tail()
+    print(x)
+    print(xs.head, xs, xs.tail, len(xs), xs.is_empty())
