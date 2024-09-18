d8c03944991ecd7f662b813a2b002b4b2cc98270
Carl <cadalebout1@ccis.edu>
2024-09-18 12:57:45 -0500

    added Edges and make changes to Vertexset

---
+++ a/a02/a02q02/Edges.py
@@ -0,0 +1,13 @@
+# File:     Edges.py
+# Dir:      a02/a02q02
+# Author:   Carl Dalebout
+
+class Edges:
+    def __init__(self):
+        pass
+    def __contains__(self, u, v):
+        raise NotImplementedError
+    def __iter__(self):
+        raise NotImplementedError
+    def __str__(self):
+        raise NotImplementedError
--- a/a02/a02q02/VertexSet.py
+++ a/a02/a02q02/VertexSet.py
@@ -34,3 +34,14 @@ if __name__ == '__main__':
     
     for _ in V:
         print(_, type(_))
+
+
+class Edges:
+    def __init__(self):
+        pass
+    def __contains__(self, u, v):
+        raise NotImplementedError
+    def __iter__(self):
+        raise NotImplementedError
+    def __str__(self):
+        raise NotImplementedError
